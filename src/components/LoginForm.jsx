fe1fa03632abdce1fff0af33db067ad3f629ccd7
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-06-16 12:31:02 -0300

    css mods

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -4,6 +4,7 @@ import {
   Flex,
   FormControl,
   FormLabel,
+  Image,
   Input,
   Link,
   Text,
@@ -18,26 +19,39 @@ export default function LoginForm() {
   return (
     <main>
       <Flex
-        bg="blue.900"
+        bg="#222435"
         align="center"
         justify="center"
-        h="100vh"
+        h="100%"
+        flexDirection="column"
       >
+        <Image
+          marginTop="80px"
+          marginBottom="20px"
+          boxSize="100px"
+          src="https://images2.imgbox.com/de/33/7SZiivv9_o.png"
+          alt="Dolphin Avatar"
+        />
         <VStack>
           <Box
-            bg="white"
-            opacity="10"
+            bgGradient="radial(#38A6D8, #35576E)"
+            opacity="0.75"
             p={10}
             rounded="md"
-            w={410}
+            w={560}
             h={400}
+            borderRadius="1em"
+            border="1px"
+            borderColor="#38A6D8"
+            marginBottom="200px"
           >
             <form onSubmit={handleSubmit}>
               <Text
-                fontSize="20px"
+                fontSize="22px"
                 fontWeight="bold"
-                color="blue.900"
+                color="#FFFFFF"
                 marginBottom="50px"
+                textAlign="center"
               >
                 Faça login em sua conta iconHype
               </Text>
@@ -45,30 +59,50 @@ export default function LoginForm() {
                 <Input
                   type="text"
                   placeholder="Insira seu e-mail"
+                  marginBottom="15px"
+                  borderRadius="1em"
+                  border="1px"
+                  borderColor="#38A6D8"
                 />
                 <Input
                   type="password"
                   placeholder="Insira sua senha"
+                  borderRadius="1em"
+                  border="1px"
+                  borderColor="#38A6D8"
                 />
               </FormControl>
-              <Text marginTop="15px">
-                <Link color="blue.900" fontWeight="bold">
+              <Text
+                fontSize="14px"
+                fontWeight="bold"
+                marginTop="10px"
+                marginTop="15px"
+              >
+                <Link color="#246483" fontWeight="bold">
                   Esqueceu sua senha?
                 </Link>
               </Text>
               <Button
-                colorScheme="facebook"
+                bg="#246483"
                 type="submit"
                 width="full"
                 marginTop="30px"
                 marginBottom="20px"
+                borderRadius="1em"
+                fontSize="18px"
+                fontWeight="bold"
               >
                 Fazer login
               </Button>
 
-              <Text marginTop="10px">
+              <Text
+                fontSize="14px"
+                fontWeight="bold"
+                marginTop="10px"
+                color="#246483"
+              >
                 Não tem uma conta icoHype?&nbsp;
-                <Link color="blue.900" fontWeight="bold">
+                <Link color="#C35B38" fontWeight="bold">
                   Crie uma aqui.
                 </Link>
               </Text>
