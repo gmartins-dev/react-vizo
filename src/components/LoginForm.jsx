98cfb53cf36b4370a5cb89821181d77342b4e859
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-06-16 09:52:22 -0300

    chakraUi installed

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,10 @@
+export default function LoginForm() {
+  return <main>
+    <form>
+          <input/>
+          <input/>
+          <button>buttonteste</button>
+        </form>
+  </main>
+  )
+}
